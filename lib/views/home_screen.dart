c0b8452a40f201fc03f61694f4430120bb905c6e
supriya gupta <supriyag75@gmail.com>
2024-07-14 12:54:18 +0530

    Added TabBarView for adding screen on tab click

--- lib/views/home_screen.dart
+++ lib/views/home_screen.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget{
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-   return Scaffold(
-     body: Container(
-         alignment : Alignment.center,
-         child: Text("Home Screen")),
-   );
+    return TabBarView(
+      children: [
+        Container(alignment: Alignment.center, color : Colors.orange, child: Text("Cabin Tab")),
+        Container(alignment: Alignment.center, color : Colors.grey, child: Text("Call Tab")),
+        Container(alignment: Alignment.center, color : Colors.lightGreenAccent, child: Text("Cake Tab")),
+      ],
+    );
   }
-
 }
